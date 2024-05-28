2c9aa2bb76d49a25496295bf188de7ddc562da26
Carlos Miguel Monteiro <c.b.monteiro@hotmail.com>
2024-05-28 01:43:12 +0100

    Refactored input parser.

--- Shell.Console/Utils/Parser.cs
+++ Shell.Console/Utils/Parser.cs
@@ -4,53 +4,24 @@ internal static class Parser
 {
     public static (string command, string[] arguments) InputParse(ReadOnlySpan<char> input)
     {
-        input = input.Trim();
+        Span<Range> parts = stackalloc Range[input.Length - 1];
 
-        var parts = Split(input);
+        var numRegions = input.Trim().Split(parts, ' ', StringSplitOptions.TrimEntries);
 
-        // The first part is the command, the rest are arguments.
-        var command = parts[0];
-        var arguments = new string[parts.Length - 1];
-        for (int i = 1; i < parts.Length; i++)
+        // The first region is the command, the rest are arguments.
+        var command = input[parts[0].Start..parts[0].End];
+
+        var numArgs = numRegions - 1;
+
+        var arguments = new string[numArgs];
+
+        for (int i = 1; i <= numArgs; i++)
         {
-            arguments[i - 1] = parts[i];
+            arguments[i - 1] = input[parts[i].Start..parts[i].End].ToString();
         }
 
         return (command.ToString(), arguments);
     }
-
-    private static ReadOnlySpan<string> Split(ReadOnlySpan<char> input)
-    {
-        var parts = new List<string>();
-        int wordStart = -1;
-
-        for (int i = 0; i < input.Length; i++)
-        {
-            if (char.IsWhiteSpace(input[i]))
-            {
-                if (wordStart != -1)
-                {
-                    parts.Add(input.Slice(wordStart, i - wordStart).ToString());
-                    wordStart = -1;
-                }
-            }
-            else
-            {
-                if (wordStart == -1)
-                {
-                    wordStart = i;
-                }
-            }
-        }
-
-        if (wordStart != -1)
-        {
-            parts.Add(input.Slice(wordStart).ToString());
-        }
-
-        return new ReadOnlySpan<string>(parts.ToArray());
-    }
-
 }
 
 //public static (string command, List<string> arguments) InputParse(string input)
@@ -59,4 +30,3 @@ internal static class Parser
 
 //    return (parts[0], parts.Skip(1).ToList());
 //}
-
